af7c863603e4bce2187ab64c80883f0cd5a312c5
Tejas Drolia <drolia.tejas2000@gmail.com>
2022-06-18 18:09:05 +0530

    Final Commit!!!

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:chitter_chatter/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(ChitterChatter());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
